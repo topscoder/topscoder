19f3c035cb6d638bed9a4a0f182eb123b0414c47
topscoder <86197446+topscoder@users.noreply.github.com>
2021-08-08 17:01:54 +0200

    Update macos-fresh-installation.sh

--- macos-fresh-installation.sh
+++ macos-fresh-installation.sh
@@ -6,14 +6,17 @@ xcode-select --install
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Install brew packages
+# Install brew packages (default -cli- applications)
 brew install git docker docker-compose mackup zsh zsh-async zsh-history-substring-search
 
-# Install brew cask packages
+# Install brew cask packages (cask = gui applications)
 brew install --cask bitwarden commander-one discord keepingyouawake kitty rectangle rocket vscodium 
 
-# nerd font
-# install oh-my-zsh ?
+# Install nerd font
+# https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FantasqueSansMono.zip
+
+# Install oh-my-zsh
+# https://ohmyz.sh/#install
 
 # Shell prompt
 # npm install -g typewritten
@@ -23,6 +26,20 @@ brew install --cask bitwarden commander-one discord keepingyouawake kitty rectan
 # - vscodium
 # - typewritten
 # - zsh?
+
+# Set zsh plugins
+# plugins=(
+#   git
+#   bundler
+#   dotenv
+#   osx
+#   rake
+#   rbenv
+#   ruby
+# )
+
+# Set zsh theme
+# https://github.com/ohmyzsh/ohmyzsh
 
 # Restore settings using mackup sync
 # mackup restore
