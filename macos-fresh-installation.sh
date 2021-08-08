7fff7dc9f858ca68d2711d2af843f016499c1358
topscoder <86197446+topscoder@users.noreply.github.com>
2021-08-08 10:45:15 +0200

    Update macos-fresh-installation.sh

--- macos-fresh-installation.sh
+++ macos-fresh-installation.sh
@@ -2,9 +2,29 @@
 
 # brew
 # brew install mackup
+# nerd font
+
+# xcode command line tools
+# amphetamine or cafeine
+# bitwarden
+# discord
+# docker
+# filezilla
 # kitty terminal
+  # noctis color scheme
+# rocket
+# rectangle
+# sublime text
+  # noctis color scheme
 # vscode
+  # noctis color scheme
 
 # install zsh
 # install oh-my-zsh
+
+# Shell prompt
+# npm install -g typewritten
+# or starship
+  # noctis color scheme
+
 # mackup restore
