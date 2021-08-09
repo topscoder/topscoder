bdd93415892a12199eb94c6c871c741d93864136
topscoder <86197446+topscoder@users.noreply.github.com>
2021-08-09 17:38:07 +0200

    New file for oh-my-zsh installation

---
+++ oh-my-zsh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install oh-my-zsh
+# https://ohmyz.sh/#install
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s $(which zsh)
+
+# Find and replace the plugins line with this one
+# See for more plugins: https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins
+# plugins=(git colored-man-pages colorize pip python brew osx)
+
+source ~/.zshrc
