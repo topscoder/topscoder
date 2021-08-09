7195f7aebd7e443e42fc1afdacdf5bf7ed852650
topscoder <86197446+topscoder@users.noreply.github.com>
2021-08-09 17:38:14 +0200

    Same

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -9,5 +9,6 @@ chsh -s $(which zsh)
 # Find and replace the plugins line with this one
 # See for more plugins: https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins
 # plugins=(git colored-man-pages colorize pip python brew osx)
+sed -i 's/\b(plugins).*?\)$\b/plugins=(git)/gi' ~/.zshrc
 
 source ~/.zshrc
