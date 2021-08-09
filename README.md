78064dac5e23933bef8b80802a97de0c866a5c23
topscoder <86197446+topscoder@users.noreply.github.com>
2021-08-09 19:16:05 +0200

    This is getting serious

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 <!--
 Semi easter egg 👀
 https://github.com/abhisheknaiidu/awesome-github-profile-readme
+
+# Load theme file
+zplug 'dracula/zsh', as:theme
 -->
 
 Developing in whatever language fits the most. Not speaking all of them though. Mostly Python, PHP, Node.js, C++. I often get a lot of inspiration by the tools, gear and other assets that are used by fellow developers. One of which is probably the most famous: https://wesbos.com/uses. At least it was my first inspiration for creating this profile here on GitHub.
--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -9,6 +9,49 @@ chsh -s $(which zsh)
 # Find and replace the plugins line with this one
 # See for more plugins: https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins
 # plugins=(git colored-man-pages colorize pip python brew osx)
-sed -i 's/\b(plugins).*?\)$\b/plugins=(git)/gi' ~/.zshrc
+
+# Zsh Plugin Manager
+brew install zplug
+
+# if [[ ! -f "$ZPLUG_HOME/init.zsh" ]]
+# then
+#     echo "Cannot find $ZPLUG_HOME/init.zsh. Exiting."
+#     exit 1
+# fi
+
+# Only if it is not already installed
+if grep -Fxq "Added by automated" ~/.zshrc
+then
+    echo "ZPLUG HOME Already added to .zshrc"
+else
+    echo "" >> ~/.zshrc
+    echo "# Added by automated macos setup tool topscoder/topscoder" >> ~/.zshrc
+    echo "export ZPLUG_HOME=/usr/local/opt/zplug" >> ~/.zshrc
+    echo "source \$ZPLUG_HOME/init.zsh" >> ~/.zshrc
+    echo "" >> ~/.zshrc
+fi
 
 source ~/.zshrc
+
+zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/colored-man-pages", from:oh-my-zsh
+zplug "plugins/pip", from:oh-my-zsh
+zplug "plugins/python", from:oh-my-zsh
+zplug "plugins/brew", from:oh-my-zsh
+zplug "plugins/osx", from:oh-my-zsh
+zplug "plugins/command-not-found", from:oh-my-zsh
+zplug "plugins/colorize", from:oh-my-zsh
+#zplug "zsh-users/zsh-syntax-highlighting"
+zplug "zsh-users/zsh-history-substring-search"
+zplug "zsh-users/zsh-completions"
+
+# FIXME
+# zplug "themes/robbyrussell", from:oh-my-zsh, as:theme
+
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
+fi
+zplug load --verbose
