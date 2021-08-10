c76aac36c4a4af3b6631cad61c9d6d66d733bbb3
topscoder <86197446+topscoder@users.noreply.github.com>
2021-08-10 21:59:52 +0200

    WIP 2.0

--- runme.sh
+++ runme.sh
@@ -1,52 +1,10 @@
 #!/bin/bash
 
-source ./utils.sh
-
-# Install XCode Developer Tools
-#      thx https://github.com/alrra/dotfiles/blob/ff123ca9b9b/os/os_x/installs/install_xcode.sh
-
-if ! xcode-select --print-path &> /dev/null; then
-
-    # Prompt user to install the XCode Command Line Tools
-    xcode-select --install &> /dev/null
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Wait until the XCode Command Line Tools are installed
-    until xcode-select --print-path &> /dev/null; do
-        sleep 5
-    done
-
-    print_result $? 'Install XCode Command Line Tools'
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Point the `xcode-select` developer directory to
-    # the appropriate directory from within `Xcode.app`
-    # https://github.com/alrra/dotfiles/issues/13
-
-    sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
-    print_result $? 'Make "xcode-select" developer directory point to Xcode'
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Prompt user to agree to the terms of the Xcode license
-    # https://github.com/alrra/dotfiles/issues/10
-
-    sudo xcodebuild -license
-    print_result $? 'Agree with the XCode Command Line Tools licence'
-
-fi
-##############################################################################################################
-
 # Ask for the administrator password upfront
 sudo -v
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Make sure everythin is working properly
-brew doctor
+# Install XCode Command Line Tools
+source ./utils.sh
 
 # Only clone if .dotfiles are not installed already
 if ! [[ $(pwd) =~ (\.dotfiles$) ]];
@@ -56,11 +14,30 @@ then
     cd .dotfiles/
 fi
 
-# Install and upgrade (by default) all dependencies from the Brewfile (.brewfile).
-brew bundle --file=.brewfile
+# Check if we are in .dotfiles folder now. It's imprtant
+if ! [[ $(pwd) =~ (\.dotfiles$) ]];
+then
+    echo
+    echo 
+    echo " ==> IT LOOKS LIKE THE .DOTFILES FOLDER IS NOT INSTALLED."
+    echo " ==> PLEASE FIX IT. "
+    echo " ==> I QUIT. "
+    echo
+    echo
+    exit 1
+fi
 
 # Unlock quarantaine mode for apps
 sh .macos-unlocker
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Make sure everythin is working properly
+brew doctor
+
+# Install and upgrade (by default) cli-tools, apps from the Brewfile (.brewfile).
+brew bundle --file=.brewfile
 
 # Install Irvue wallpaper manager from App Store
 mas install 1039633667
@@ -68,20 +45,52 @@ mas install 1039633667
 # FIXME
 #./oh-my-zsh.sh
 
-# System settings
+# Git configuration
+echo 
+echo 
+echo " 👉👉👉 Git configuration (.gitsettings)"
+echo 
+echo 
+
+sh .gitsettings
+
+
+echo 
+echo 
+echo " 👉👉👉 macOS configuration (.macos*)"
+echo 
+echo 
+
 sh .macos
 sh .macos-user-defaults
 sh .macos-paulirish
-python3 .macos-dock
+sh .macos-dock
 
-# Application specific settings
-sh .codium
+
+echo 
+echo 
+echo " 👉👉👉 Reload applications"
+echo 
+echo 
+
+killall Dock
+killall Finder
+
+# VSCodium extensions
+echo 
+echo 
+echo " 👉👉👉 VSCodium extensions (.vscodium)"
+echo 
+echo 
+
+sh .vscodium
 
 # Done
 echo
-echo "-- Installation done --"
 echo
-echo "Don't forget to use `mackup restore`"
+echo " 👉👉👉 INSTALLATION DONE"
 echo
-echo "-- KTHXBAI --"
+echo "Don't forget to run `mackup restore`"
+echo
+echo " 👉👉👉 KTHXBAI"
 echo
