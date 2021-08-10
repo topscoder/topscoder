fcd032adc74a4c4f953b3de0d10d58e96dd1629a
topscoder <86197446+topscoder@users.noreply.github.com>
2021-08-10 15:23:48 +0200

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-source './functions.sh'
-
 # Install XCode Developer Tools
 #      thx https://github.com/alrra/dotfiles/blob/ff123ca9b9b/os/os_x/installs/install_xcode.sh
+
+print_in_green() {
+    printf "\e[0;32m$1\e[0m"
+}
+
+print_success() {
+    print_in_green "  [✔] $1\n"
+}
+
+print_error() {
+    print_in_red "  [✖] $1 $2\n"
+}
+
+print_in_red() {
+    printf "\e[0;31m$1\e[0m"
+}
+
+print_result() {
+    [ $1 -eq 0 ] \
+        && print_success "$2" \
+        || print_error "$2"
+
+    return $1
+}
 
 if ! xcode-select --print-path &> /dev/null; then
 
